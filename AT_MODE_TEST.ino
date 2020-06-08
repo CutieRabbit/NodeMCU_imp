07d31951e3dc166cab30fd6517abf14d3f25a07e
Xuan <52220077+CutieRabbit@users.noreply.github.com>
2020-06-08 14:44:59 +0800

    Create AT_MODE_TEST.ino

---
+++ AT_MODE_TEST.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(6, 7);
+char val;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("BT is ready!");
+
+  BT.begin(38400);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    val = Serial.read();
+    BT.print(val);
+  }
+
+  if (BT.available()) {
+    val = BT.read();
+    Serial.print(val);
+  }
+}
